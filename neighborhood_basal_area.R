28a8e75eceb8f7c9977056985a477f8b2484d559
mcgregorian1 <mcgregori@si.edu>
2019-03-15 10:23:57 -0400

    Finished calculating intial NBA

--- neighborhood_basal_area.R
+++ neighborhood_basal_area.R
@@ -1,14 +1,20 @@
-# calculating basal area increment for ForestGEO plot trees
+# calculating neighborhood basal area (NBA) for ForestGEO plot trees
 
+#this script is attempting to recreate the efforts done by Alan Tepley from Gonzalez et al 2016: https://esajournals.onlinelibrary.wiley.com/doi/epdf/10.1002/ecs2.1595, whereby neighborhood basal area was calculated by summing the basal area of all trees within a given distance of a focal tree. Specifically, basal area for this paper was calculated to 30m at a distance increment of 0.5m. Initial calculations were done in Excel.
+
+#for trees that have >1 stem that is at least 10cm dbh (1 individual in 2013), Tepley said, "When I calculated neighborhood basal area for the tree with Tag Number 60108 and Stem Tag 1, I would exclude the basal area for that stem and include the basal area for the tree with Tag Number 60108 and Stem Tag 2, and the distance to that tree is 0 meters."
+
+#Tepley also created a calculation such that if a focal tree was within 30m of the ForestGEO plot edge, then that distance away he would give the NBA to be 0. For example, if a tree was 15m from the ForestGEO edge, that tree would have 0 NBA until distance 15.5m. This has not been replicated in this script, but it should be acknowledged that this was the original intent.
+
+#script
 scbi.full2 <- read.csv("C:/Users/mcgregori/Dropbox (Smithsonian)/Github_Ian/SCBI-ForestGEO-Data/tree_main_census/data/census-csv-files/scbi.full2.csv", stringsAsFactors=FALSE)
 
 scbi.full2[5340, 3] <- 40874 #duplicated tag (above 10cm dbh)
-scbi.full2 <- scbi.full2[scbi.full2$dbh>=100  & !grepl("S", scbi.full2$codes), ]
+scbi.full2 <- scbi.full2[scbi.full2$dbh>=100, ]#& !grepl("S", scbi.full2$codes), ]
 
 trees <- scbi.full2[c(3,7,8)]
 rownames(trees) <- trees[,1]
 trees <- trees[, -1]
-
 
 library(vegan)
 d <- vegdist(trees, method="euclidean") #calculate distance between one tree and all the other trees using Pythagorean theorem
@@ -40,13 +46,22 @@ scbi.sub_list <- lapply(scbi.sub$tag, function(x){
 names(scbi.sub_list) <- scbi.sub$tag
 
 
-dist_shift <- dist[-1]
+dist_shift <- dist[-1] #get rid of x0 now that you've defined it
 
+#in short, the loop below does the following:
+#1. defines a focal tree, creates df "test" with all distances of that focal tree (line 52-54)
+#2. Adds column to test with all the trees the focal is relating to, plus the basal area of each one (line 57-62)
+#3. Split test into separate dataframes (test_list) to make it easier to work with (line 64-66)
+#4. Filter scbi.sub_list by the focal tree to "z". (line 69)
+#5. Make each element of test_list a separate df "w". Then, define column indices to use (inc, inc_num, inc_prev). For each increment (inc and inc_num), subset the df "w" so that it returns all trees with distances less than the number of inc_num. Then, fill in df "z" whereby if inc_num is less than the max distance in the subset "w", fill in that column in "z" with the value of inc_prev. Otherwise, if inc is greater than the max distance, add the basal area from x0 to the sum of the basal areas in the filtered "w" df. (lines 72-85)
+#6. Add z to list "full", then rbind to one df "all_dist". (lines 87-90)
+
+all_dist <- NULL
+full <- list()
 for (j in seq(along=unique(simple$tree1))){
-  tree <- unique(simple$tree1)[[j]]
+  tree <- unique(simple$tree1)[[j]] #one tree at a time
   test <- simple[simple$tree1 == tree | simple$tree2 == tree, ] #filter by tree
   test <- test[order(test$distance), ] #order by distance
-  test$diff <- 
   
   sapply(1:nrow(test), function(x){
     test$diff <- ifelse(test[x,1] == tree, test[x,2], 
@@ -59,46 +74,27 @@ for (j in seq(along=unique(simple$tree1))){
     test[test$diff == x, ] }) #split test into separate df
   names(test_list) <- unique(test$diff)
 
+  z <- scbi.sub_list[[grep(paste0("^",tree,"$"), names(scbi.sub_list))]]
     
-    z <- scbi.sub_list[[grep(tree, scbi.sub_list)]]
+  for (q in seq(along=test_list)){
+    w <- test_list[[q]]
+    w$distance <- as.numeric(w$distance)
     
-#the below two loops work. The next step is to do some rbinding and figure make sure this entire thing then works.
-    for (q in seq(along=test_list)){
-      w <- test_list[[q]]
-      w$distance <- as.numeric(w$distance)
+    for (i in seq(along=dist_shift)){
+      inc <- dist_shift[[i]]
+      inc_num <- gsub("x", "", inc)
+      inc_num <- as.numeric(inc_num)
+      inc_num_prev <- inc_num - 0.5
+      inc_prev <- gsub("^", "x", inc_num_prev)
       
-      for (i in seq(along=dist_shift)){
-        inc <- dist_shift[[i]]
-        inc_num <- gsub("x", "", inc)
-        inc_num <- as.numeric(inc_num)
-        inc_num_prev <- inc_num - 0.5
-        inc_prev <- gsub("^", "x", inc_num_prev)
-        
-        w <- test[test$distance < inc_num, ]
-        
-          
-        z[, inc] <- ifelse(inc_num < max(w$distance), z[, inc_prev], sum(z$x0, sum(w$basal_diff)))
-      }
-  }
-#        
-       
-        
-        scbi.sub$inc <- ifelse(scbi.sub$tag == tree & test_list[[x]]$distance >= inc_num, 
-                               sum(inc-1, )
-        
-        
-        inc <- colnames(scbi.sub[, 6:ncol(scbi.sub)])[[i]]
-        #inc_prev <- colnames(scbi.sub[[i-1]])
-        
-        scbi.sub$inc <- 
-      }
+      w <- test[test$distance < inc_num, ]
+      
+      z[, inc] <- ifelse(inc_num < max(w$distance), z[, inc_prev], sum(z$x0, sum(w$basal_diff)))
+      full[[j]] <- z
     }
   }
+  all_dist <- rbind(all_dist, full[[j]])
 }
-
-apply(6:ncol(scbi.sub), function(x){
-  ifelse()
-})
 
 
 
