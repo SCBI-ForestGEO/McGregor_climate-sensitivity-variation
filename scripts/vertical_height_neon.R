89347b11a29246b4ab762726166cfe39d9b26b01
Ian McGregor <mcgregori@si.edu>
2019-06-13 16:47:02 -0400

    Update vertical_height_neon.R

--- scripts/vertical_height_neon.R
+++ scripts/vertical_height_neon.R
@@ -3,25 +3,31 @@
 library(neonUtilities)
 library(plotly)
 library(lubridate)
+library(dplyr)
+library(data.table)
 
 #traits from NEON
 ##1. single aspirated air temp: DP1.00002.001, avg = 1min or 30min
 ##2. 2-D wind speed and direction: DP1.00001.001, avg = 2min or 30min
 ##3. IR Biological Temp (infrared): DP1.00005.001, avg = 1min or 30min
 ##4. Relative humidity: DP1.00098.001, avg = 1min or 30min
-##5. Shortwave radiation
+##5. Shortwave radiation: DP1.00022.001, avg = 1min or 30min
 
 #this command will load the data into R and collapse into one df (within a list).
 #it will not download/store anything on the computer, but working with large dfs will run slowly. Hence, it is a good idea to look at the 30min avg first.
 
-dp <- data.frame("data" = c("SAAT", "wind", "biotemp", "RH"),
-                 "id" = c("DP1.00002.001", "DP1.00001.001", "DP1.00005.001", "DP1.00098.001"),
-                 "value" = c("tempSingleMean", "windSpeedMean", "bioTempMean", "RHMean"))
+dp <- data.frame("data" = c("SAAT", "wind", "biotemp", "RH", "SR"),
+                 "id" = c("DP1.00002.001", "DP1.00001.001", "DP1.00005.001", "DP1.00098.001", "DP1.00022.001"),
+                 "value" = c("tempSingleMean", "windSpeedMean", "bioTempMean", "RHMean", "shortRadMean"))
+
+date <- data.frame("year" = c(rep(2018, 4), rep(2017, 4), rep(2016, 4), rep(2015, 4)),
+                    "month" = c(rep(5:8, 4)))
+
 
 dp[] <- lapply(dp, as.character)
 
 #this loop for some reason isn't producing plotly graphs that will work, but everything else runs smoothly
-for (i in seq(along=1:4)){
+for (i in seq(along=1:5)){
   value <- dp$value[[i]]
   
   neon_tower <- loadByProduct(dpID=dp$id[[i]], 
@@ -34,20 +40,22 @@ for (i in seq(along=1:4)){
   
   neon_data <- neon_tower[[1]]
   neon_data_sub <- neon_data[colnames(neon_data) %in% c("verticalPosition", "startDateTime", value)]
-  # neon_data_sub$startDateTime <- ymd_hms(as.character(neon_data_sub$startDateTime))
   
+  #reformat dates
+  # neon_data_sub$startDateTime <- ymd_hms(as.character(neon_data_sub$startDateTime))
   neon_data_sub$startDateTime <- as.character(neon_data_sub$startDateTime)
   neon_data_sub$startDateTime <- gsub("T", " ", neon_data_sub$startDateTime)
   neon_data_sub$startDateTime <- as.POSIXct(neon_data_sub$startDateTime, format = "%Y-%m-%d %H:%M:%OS")
   
+  #graph with plotly
   y <- list(title = value)
   
   assign(paste0(dp$data[[i]], "_plot"), 
          plot_ly(data = neon_data_sub, x = ~startDateTime, y = ~neon_data_sub[, value], type = "scatter", color = ~verticalPosition, mode = "markers") %>%
            layout(yaxis = y))
   
+  #graph with ggplot
   neon_data_sub$verticalPosition <- as.character(neon_data_sub$verticalPosition)
-  
   assign(paste0(dp$data[[i]], "_graph"), 
          ggplot(data = neon_data_sub) +
            aes_string(x = colnames(neon_data_sub)[2], y = colnames(neon_data_sub)[3]) +
@@ -59,11 +67,23 @@ SAAT_plot
 wind_plot
 biotemp_plot
 RH_plot
+SR_plot
 
+neon_data_sub$day <- substr(neon_data_sub$startDateTime, 1, nchar(neon_data_sub$startDateTime)-0)
 
-value
-gsub(\, "", value)
-as.name(value)
+#determine threshold for sunny/cloudy day
+q <- neon_data_sub %>%
+      group_by(day) %>%
+      summarize(total_Rad = sum(shortRadMean))
+
+boxplot(q$total_Rad)
+ggplot(q) +
+  aes(x = day, y = total_Rad) +
+  geom_point(color = "#0c4c8a") +
+  theme_minimal()
+
+esquisser()
+
 
 
 
