f4894800221bd4a444c4a2e836afe81fdf44c446
Valentine Herrmann <herrmannv@si.edu>
2019-06-25 16:08:08 -0400

    change tif to jpg

--- scripts/plot_time_series.R
+++ scripts/plot_time_series.R
@@ -54,7 +54,7 @@ pre_drought_years <- drought_years - 5
 
 
 if(save.plots)  {
-  tiff(paste0("manuscript/tables_figures/Time_series_for_each_species.tif"), res = 300, width = 150, height = 150, units = "mm", pointsize = 10)
+  jpeg(paste0("manuscript/tables_figures/Time_series_for_each_species.jpg"), res = 300, width = 150, height = 150, units = "mm", pointsize = 10)
 }
 
 par(mfrow = c(length(SPECIES_IN_ORDER) + 3, 1), mar = c(0,0,0,0), oma = c(4, 6, 0, 0))
