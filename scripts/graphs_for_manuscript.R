6bc7838df62e8707b7cb282c582222f8eccbe84a
Ian McGregor <mcgregori@si.edu>
2019-07-23 14:05:47 -0400

    Update graphs_for_manuscript.R

--- scripts/graphs_for_manuscript.R
+++ scripts/graphs_for_manuscript.R
@@ -3,16 +3,17 @@
 # Developed by: Ian McGregor - mcgregori@si.edu
 # R version 3.5.3 - First created July 2019
 ######################################################
-library(ggplot2) #2
-library(RCurl) #2
-library(tidyr) #2
-library(grid) #2
-library(gridExtra) #2
+library(ggplot2)
 
 #1 NEON vertical height profiles
 source('scripts/vertical_height_neon.R', echo=TRUE)
 #########################################################################
 #2 height by crown position in 2018 ####
+library(RCurl) #2
+library(tidyr) #2
+library(grid) #2
+library(gridExtra) #2
+
 trees_all <- read.csv("manuscript/tables_figures/trees_all.csv", stringsAsFactors = FALSE)
 
 scbi.stem3 <- read.csv(text=getURL("https://raw.githubusercontent.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/master/tree_main_census/data/census-csv-files/scbi.stem3.csv"), stringsAsFactors = FALSE)
@@ -51,15 +52,103 @@ current_ht <- current_ht[order(current_ht$tree, current_ht$year), ]
 current_ht <- current_ht[!is.na(current_ht$position_all), ]
 current_ht$position_all <- factor(current_ht$position_all, levels = c("dominant", "co-dominant", "intermediate", "suppressed"))
 
+quantile(current_ht$height.m, c(.95), na.rm=TRUE) #95% quantile = 35.002m
+
+quant <- data.frame(yintercept = 35.0022, Lines = "95th percentile")
+
 heights <-
   ggplot(data = current_ht) +
   aes(x = position_all, y = height.m, group = position_all) +
   # aes(x=position_all, y=height.m, fill=year) +
   geom_boxplot() +
-  ggtitle("Current height vs crown position")+
+  ggtitle("Height vs crown position 2018") +
   xlab("crown position") +
   ylab("height(m)") +
   scale_y_continuous(breaks = scales::pretty_breaks(n = 6), limits=c(0,60)) +
   theme_minimal()
-  
 
+#get height data for all trees >10cm dbh in census
+scbi.stem3 <- read.csv(text=getURL("https://raw.githubusercontent.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/master/tree_main_census/data/census-csv-files/scbi.stem3.csv"), stringsAsFactors = FALSE)
+
+scbi.stem3$dbh <- as.numeric(scbi.stem3$dbh)
+scbi.stem3$dbh <- ifelse(is.na(scbi.stem3$dbh), 0, scbi.stem3$dbh)
+
+scbi18_ht <- scbi.stem3
+scbi18_ht <- scbi18_ht[scbi18_ht$dbh>=100, ]
+scbi18_ht <- scbi18_ht[,c(2:5,11,14:15)]
+
+scbi18_ht$dbh_old.cm <- scbi18_ht$dbh/10
+scbi18_ht$dbh.ln.cm <- log(scbi18_ht$dbh_old.cm)
+
+#linear log-log regression
+#the full equation is using all points for which we have data to create the equation, despite that for several species we don't have enough data to get a sp-specific equation
+scbi18_ht$height.ln.m <- 
+   ifelse(scbi18_ht$sp == "caco", (0.348+0.808*scbi18_ht$dbh.ln.cm),
+   ifelse(scbi18_ht$sp == "cagl", (0.681+0.704*scbi18_ht$dbh.ln.cm),
+   ifelse(scbi18_ht$sp == "caovl", (0.621+0.722*scbi18_ht$dbh.ln.cm),
+   ifelse(scbi18_ht$sp == "cato", (0.776+0.701*scbi18_ht$dbh.ln.cm),
+   ifelse(scbi18_ht$sp == "fagr", (0.708+0.662*scbi18_ht$dbh.ln.cm),
+    ifelse(scbi18_ht$sp == "fram", (0.715+0.619*scbi18_ht$dbh.ln.cm),
+    ifelse(scbi18_ht$sp == "juni", (1.22+0.49*scbi18_ht$dbh.ln.cm),
+    ifelse(scbi18_ht$sp == "litu", (1.32+0.524*scbi18_ht$dbh.ln.cm),
+    ifelse(scbi18_ht$sp == "qual", (1.14+0.548*scbi18_ht$dbh.ln.cm),
+    ifelse(scbi18_ht$sp == "qupr", (0.44+0.751*scbi18_ht$dbh.ln.cm),
+    ifelse(scbi18_ht$sp == "quru", (1.17+0.533*scbi18_ht$dbh.ln.cm),
+    ifelse(scbi18_ht$sp == "quve", (0.864+0.585*scbi18_ht$dbh.ln.cm),
+                                  (0.791+0.645*scbi18_ht$dbh.ln.cm)))))))))))))
+
+scbi18_ht$height.m <- exp(scbi18_ht$height.ln.m) #used below in #4
+
+#######################################################################################
+#4 Get mean leaf traits as function of TWI ####
+library(ggstance)
+
+scbi.stem3$dbh <- ifelse(is.na(scbi.stem3$dbh), 0, scbi.stem3$dbh)
+scbi.stem3 <- scbi.stem3[scbi.stem3$dbh >= 100, ] #>10cm dbh
+
+#get utm coords
+source('D:/Dropbox (Smithsonian)/Github_Ian/SCBI-ForestGEO-Data/R_scripts/SIGEO_plot_grid_UTMcoord.R', echo=TRUE)
+
+plot_to_UTM(scbi.stem3)
+scbi18 <- sigeo_coords
+scbi18 <- scbi18[, c(2:5,20:21)]
+scbi18_x <- scbi18[, c(5:6)]
+topo <- raster("data/plot_TWI.tif")
+twi <- extract(topo, scbi18_x, method="simple")
+scbi18$TWI <- twi
+
+#add in leaf traits (no rp because it's categorical)
+leaf_traits <- read.csv(text=getURL("https://raw.githubusercontent.com/EcoClimLab/HydraulicTraits/master/data/SCBI/processed_trait_data/SCBI_all_traits_table_species_level.csv?token=AJNRBEJQMFQQQHLV5MJVM7K5HCCRC"), stringsAsFactors = FALSE)
+
+leaf_traits <- leaf_traits[, c(1,8,12,26,28)]
+
+for (i in seq(along=2:ncol(leaf_traits))){
+   trait <- colnames(leaf_traits[2:ncol(leaf_traits)])
+   scbi18[, trait[[i]]] <- leaf_traits[, trait[[i]]][match(scbi18$sp, leaf_traits$sp)]
+}
+
+scbi18 <- scbi18[complete.cases(scbi18), ]
+scbi18$height.m <- scbi18_ht$height.m[match(scbi18$stemID, scbi18_ht$stemID)]
+
+#graph results
+plot(scbi18$TWI, scbi18$mean_TLP_Mpa)
+
+ggplot(scbi18) +
+   aes(x = TWI, y = mean_TLP_Mpa, colour = sp) +
+   geom_boxploth() +
+   scale_color_hue() +
+   theme_minimal()
+
+ggplot(scbi18) +
+   aes(x = TWI, y = height.m) +
+   geom_point(size = 1L, colour = "#0c4c8a") +
+   theme_minimal() +
+   facet_wrap(vars(sp))
+#######################################################################################
+#5 Add the graphs together ####
+quantile(current_ht$height.m, c(.95), na.rm=TRUE) #95% quantile = 35.002m
+
+quant <- data.frame(yintercept = 35.0022, Lines = "95th percentile")
+
+#add this part to each graph:
+geom_hline(aes(yintercept = yintercept, linetype = "dashed"), quant)
